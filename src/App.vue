cff2facd9ed0cefc1cd38a46bea94dd7c075f220
ljf20122010 <850224665@qq.com>
2020-11-18 20:36:05 +0800

    11/18

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,52 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{message}}
+    <div :id="message">
+      <ul>
+        <todo-item @delete="handleDelete" v-for="item in list" :key="item.title" :title="item.title" :del="item.del">
+          <template v-slot:pre="{value}">
+            <span slot="pre">前置图标,{{value}}</span>
+          </template>
+          <template v-slot:sub>
+            <span slot="sub">后置图标</span>
+          </template>
+
+        </todo-item>
+      </ul>
+      <todo-list></todo-list>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "@/components/TodoList";
+import TodoItem from "@/components/TodoItem";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoList,
+    TodoItem
+  },
+  data(){
+    return {
+      message:'hello,world',
+      list:[{
+        title:'class1',
+        del:true
+      },{
+        title:'class2',
+        del:true
+      }],
+    }
+  },
+  methods:{
+    handleDelete(val){
+      console.log('handDelete',val)
+    }
   }
 }
 </script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+  <slot name="pre" :value="value"></slot>
+  <span v-if="del">{{title}}</span>
+  <span v-else style="text-decoration: line-through">{{title}}</span>
+  <slot name="sub"></slot>
+  <button v-show="del" @click="handleclick">删除</button>
+</li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    del:{
+      type:Boolean,
+      default:true,
+    },
+  },
+  data:function (){
+    return {
+      value:Math.random()
+    }
+  },
+  methods:{
+    handleclick(){
+      console.log('点击删除按钮')
+      this.$emit('delete',this.title)
+    }
+  },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
